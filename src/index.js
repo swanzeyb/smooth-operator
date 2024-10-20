9567d2867215dbebd72f707c43c1fce91f24f76d
Benjamin Swanzey <swanzeyb2001@gmail.com>
2024-10-19 19:01:05 -0700

    Configure OpenAI

--- src/index.js
+++ src/index.js
@@ -1,33 +1,56 @@
-const { Client, GatewayIntentBits } = require('discord.js')
+import OpenAI from 'openai'
+const openai = new OpenAI()
 
-// Create a new client instance
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.DirectMessages,
-    GatewayIntentBits.MessageContent,
-  ],
-})
-
-// When the bot is ready
-client.once('ready', async () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-
-  // Fetch the user by their ID
-  const user = await client.users.fetch(process.env.USER_ID)
-
-  if (user) {
-    user.send('Hello! This is a DM from your bot!')
+function getThread({ threadId }) {
+  if (threadId) {
+    return openai.beta.threads.retrieve(threadId)
+  } else {
+    return openai.beta.threads.create()
   }
-})
+}
 
-// Command to send a DM when triggered (for example: !dm)
-client.on('messageCreate', async (message) => {
-  if (message.content === '!dm') {
-    const user = message.author
-    await user.send('Hey! I am sending you a DM!')
+function getAssistant({ assistantId }) {
+  if (assistantId) {
+    return openai.beta.assistants.retrieve(assistantId)
+  } else {
+    return openai.beta.assistants.create({
+      name: 'Shower Thought Helper',
+      instructions:
+        'You are a personal assitant. Your job is to read my incoming messages and store them in Notion. You can also help me with my thoughts and ideas.',
+      tools: [],
+      model: 'gpt-4o-mini',
+    })
   }
-})
+}
 
-// Log in to Discord with your bot token
-client.login(process.env.BOT_TOKEN)
+await (async () => {
+  console.info('--- Bot is now starting ---')
+
+  // Read the config
+  const file = Bun.file('src/config.json')
+  const config = await file.json()
+
+  // Get the assistant
+  const assistant = await getAssistant({ assistantId: config.assistantId })
+  console.log(`Assistant ID: ${assistant.id}`)
+
+  // Save the assistant ID
+  if (!config.assistantId) {
+    config.assistantId = assistant.id
+
+    await Bun.write(file, JSON.stringify(config, null, 2))
+  }
+
+  // Get the thread
+  const thread = await getThread({ threadId: config.threadId })
+  console.log(`Thread ID: ${thread.id}`)
+
+  // Save the thread ID
+  if (!config.threadId) {
+    config.threadId = thread.id
+
+    await Bun.write(file, JSON.stringify(config, null, 2))
+  }
+
+  console.info('--- Bot is now ready ---')
+})()
